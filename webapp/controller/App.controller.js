7615e91a5d1e59908158ae29aff3fc78bf2fbd90
Gopal Nair <gnair@costco.com>
2017-10-20 14:18:28 +0000

    Initial Version , Week 1 Unit 3

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,51 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function(Controller,MessageToast) {
+	"use strict";
+
+	return Controller.extend("opensap.myapp.controller.App", {
+
+		onShowHello:function(){
+			MessageToast.show("Hello openSAP");
+		}
+		
+
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf view.testView
+		 */
+		//	onInit: function() {
+		//
+		//	},
+
+		/**
+		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+		 * (NOT before the first rendering! onInit() is used for that one!).
+		 * @memberOf view.testView
+		 */
+		//	onBeforeRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+		 * This hook is the same one that SAPUI5 controls get after being rendered.
+		 * @memberOf view.testView
+		 */
+		//	onAfterRendering: function() {
+		//
+		//	},
+
+		/**
+		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+		 * @memberOf view.testView
+		 */
+		//	onExit: function() {
+		//
+		//	}
+
+	});
+
+});
